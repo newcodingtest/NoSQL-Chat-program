2a941974ee3edc5262ab4b62aa4b6b4bb3a4b74e
newcodingtest <pulpul8282@naver.com>
2021-11-14 23:01:36 +0900

    Initial commit

---
+++ src/main/java/com/yoon/Chat.java
@@ -0,0 +1,21 @@
+package com.yoon;
+
+import lombok.Data;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@Document(collection = "chat")
+public class Chat {
+    @Id
+    private String id;
+    private String msg;
+    private String sender;
+    private String receiver;
+
+    private LocalDateTime createAt;
+}
---
+++ src/main/java/com/yoon/ChatController.java
@@ -0,0 +1,30 @@
+package com.yoon;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@RestController
+public class ChatController {
+
+    private final ChatRepository chatRepository;
+
+    @GetMapping(value="sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver){
+        return chatRepository.mFindBySender(sender,receiver)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @PostMapping("/chat")
+    public Mono<Chat> setMsg(@RequestBody Chat chat){ // 데이터 한건 리턴:Mono 여러건:Flux
+        chat.setCreateAt(LocalDateTime.now());
+        return chatRepository.save(chat);
+    }
+}
