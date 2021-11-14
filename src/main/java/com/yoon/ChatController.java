f0dfca548f9edae2eb26c3464ac569d38c105bfe
newcodingtest <pulpul8282@naver.com>
2021-11-15 05:43:14 +0900

    rest api 채팅 송수신 컨트롤러

--- src/main/java/com/yoon/ChatController.java
+++ src/main/java/com/yoon/ChatController.java
@@ -16,15 +16,17 @@ public class ChatController {
 
     private final ChatRepository chatRepository;
 
+    @CrossOrigin
     @GetMapping(value="sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver){
         return chatRepository.mFindBySender(sender,receiver)
                 .subscribeOn(Schedulers.boundedElastic());
     }
 
+    @CrossOrigin
     @PostMapping("/chat")
     public Mono<Chat> setMsg(@RequestBody Chat chat){ // 데이터 한건 리턴:Mono 여러건:Flux
         chat.setCreateAt(LocalDateTime.now());
-        return chatRepository.save(chat);
+        return chatRepository.save(chat); //Object를 리턴하면 자동으로 JSON 변환(MessageConverter)
     }
 }
